22f0943290954db0126eb8432bb7accab7225110
Michael Gee <michaelgee221@gmail.com>
2016-10-09 11:24:13 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@
 
 //String is to hold values and keep numbers together
 var currentInput = "";
+var currentEntry = [];
 var currentInputArr = [];
 
 //AC (Clear) Button Function
@@ -13,31 +14,48 @@ function clearInput(ac){
   
   // clear current input back to empty string
   currentInput = "";
-  currentEntry = "";
+  currentEntry = [];
   currentInputArr = [];
   
-}
+} // clearInput() bracket end
 
 // Number Button Function
 function buttonPress(button) {
+  var operators = ["x", "+", "-", "/"]
   var regex = /[^\S]/g;  
   var currentButton = document.getElementById(button).innerHTML; 
    //gets rid of all whitespace
   currentButton = currentButton.replace(regex, "");
-  //ParseInt button press to make sure Button is a number
-  var numCheck = parseInt(currentButton);
   
-  if(numCheck === NaN) {
-    currentInputArr.push(currentInput);
-
-  } else {
-    currentInput += (currentButton);
+  var checkIfOperator = operators.indexOf(currentButton);
+  
+  // When a number is pressed
+  if (checkIfOperator === -1) {
+    
+    if(operators.indexOf(currentEntry[currentEntry.length -1]) !== -1){
+      currentInput = "";
+    }
+    
+    currentInput += currentButton;
+    currentEntry.push(currentButton);
   
     //Current Input Display
-    document.getElementById("current-entry").innerHTML = currentInput;
+    document.getElementById("current-entry").innerHTML = currentEntry.length === 0 ? "0" : currentEntry.join("");
+    document.getElementById("input").innerHTML = currentInput;    
+  } else { //when an operator is pressed
+    currentInputArr.push(currentInput);
+    
+    currentInput = "";
+    currentInput += currentButton;
+    currentEntry.push(currentButton);
+    document.getElementById("input").innerHTML = currentButton;
+    
+    document.getElementById("current-entry").innerHTML = currentEntry.join("");
+    
   }
    
   console.log(currentInput);
+  console.log(currentEntry);
   console.log(currentInputArr);
   
 }
