85701914a9da49e4f70ac6809a25c531e99e5fe6
Michael Gee <michaelgee221@gmail.com>
2016-10-08 23:41:50 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,43 @@
-var currentInput = [];
+//Current Entry String and Arr to Be Joined into String
 
+//String is to hold values and keep numbers together
+var currentInput = "";
+var currentInputArr = [];
+
+//AC (Clear) Button Function
 function clearInput(ac){
   
   document.getElementById("input").innerHTML = 0;
   
-}
-
-function operatorPress(operator){
+  document.getElementById("current-entry").innerHTML = 0;
+  
+  // clear current input back to empty string
+  currentInput = "";
+  currentInputArr = [];
   
 }
 
-
-function numPress(num) {
+// Number Button Function
+function buttonPress(button) {
+  var regex = /[^\S]/g;  
+  var currentButton = document.getElementById(button).innerHTML; 
   
-  var regex = /[^\w]/g;  
-  var currentInput = document.getElementById(num).innerHTML;
-    currentInput = currentInput.replace(regex, "");
+  // Check if button pressed was a number, if not will return NaN
+  var operatorCheck = parseInt(currentButton);
+  
+  if(operatorCheck === NaN){
+    
+  }
+  
+  console.log(operatorCheck);
+  
+  //gets rid of all whitespace
+  currentButton = currentButton.replace(regex, "");
+  
+  currentInput += (currentButton);
+  
+  //Current Input Display
+  document.getElementById("current-entry").innerHTML = currentInput;
   
   console.log(currentInput);
   
